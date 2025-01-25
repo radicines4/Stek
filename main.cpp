50f49fc307c73f6b40a5321e70e04a5c5f2df2d9
radicines4 <r4dicines@gmail.com>
2025-01-25 20:25:44 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n;
+    stack <int> s;
+    stack <int> m;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x;
+        s.push(x);
+    }
+    if(!s.empty())
+    {
+        while (!s.empty()){
+            m.push(s.top());
+            s.pop();
+        }
+        while(!m.empty()){
+            cout << m.top() << " ";
+            m.pop();
+        }
+    } else cout << "Stek je prazan";
+    return 0;
+}
